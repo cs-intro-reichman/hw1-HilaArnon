0665f01945412eee6ba3991e0f0afba563674f25
HilaA77 <barhila77@gmail.com>
2024-11-06 20:41:56 +0200

    I finished

--- Ascend.java
+++ Ascend.java
@@ -15,11 +15,11 @@ public class Ascend {
 
 	public static void main(String[] args) {
 		int lim = Integer.parseInt(args[0]);
-		int num1 = (int)(Math.random() * lim);
-		int num2 = (int)(Math.random() * lim);
-		int num3 = (int)(Math.random() * lim);
+		int num1 = (int) (Math.random() * lim);
+		int num2 = (int) (Math.random() * lim);
+		int num3 = (int) (Math.random() * lim);
 		System.out.println(num1 + " " + num2 + " " + num3);
-		
+
 		int max = Math.max(num1, num2);
 		max = Math.max(max, num3);
 		int min = Math.min(num1, num2);
